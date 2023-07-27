6078c6ec0ba03c2665febd78b41da77c15d76b1b
Lorenzo Ferron <lorenzo_ferron@hotmail.it>
2023-07-27 19:27:40 +0200

    demangling

--- src/anfora/agent/not_used/swift.ts
+++ src/anfora/agent/not_used/swift.ts
@@ -1,7 +1,16 @@
 const GRDB_PATH: string = Process.enumerateModules().find((x: Module): boolean => x.name === "GRDB")!.path;
 
-Interceptor.attach(Module.getExportByName(GRDB_PATH, "$s4GRDB8DatabaseC13usePassphraseyy10Foundation4DataVKF"), {
-    onEnter(): void {
+declare let Swift: any;
 
-    }
-});
+if (Swift.available) {
+    // Tested on iOS 14.4.2. and iOS 15.1b1.
+    const mangled: string = "$s4GRDB8DatabaseC13usePassphraseyy10Foundation4DataVKF";
+    const demangled: NativePointer = Swift.api.swift_demangle(Memory.allocUtf8String(mangled), mangled.length, NULL, NULL, 0);
+    console.log(`Function hooked: ${demangled.readUtf8String()}`);
+
+    const listener = Interceptor.attach(Module.getExportByName(GRDB_PATH, mangled), {
+        onEnter(args) {
+            listener.detach();
+        },
+    });
+}
