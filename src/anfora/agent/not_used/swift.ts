877f5a8af6da95ae5bfac50fa528122b70faeaef
Lorenzo Ferron <lorenzo_ferron@hotmail.it>
2023-07-31 00:35:22 +0200

    Tested `swift.ts` on iPadOS 17: IT WORKS!

--- src/anfora/agent/not_used/swift.ts
+++ src/anfora/agent/not_used/swift.ts
@@ -6,7 +6,7 @@ const GRDB_PATH: string = Process.enumerateModules().find((x: Module): boolean =
 declare let Swift: any;
 
 if (Swift.available) {
-    // Tested on iOS 14.4.2 and iOS 15.1b1.
+    // Tested on iOS 14.4.2, iOS 15.1b1 and iOS 17.0b4.
     const mangled: string = "$s4GRDB8DatabaseC13usePassphraseyy10Foundation4DataVKF";
     const demangled: NativePointer = Swift.api.swift_demangle(Memory.allocUtf8String(mangled), mangled.length, NULL, NULL, 0);
     console.log(`Function hooked: ${demangled.readUtf8String()}`);
