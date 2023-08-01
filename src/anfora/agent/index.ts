3ee004b0301e7287c1b0e5ee950d4b6d4adffbc0
Lorenzo Ferron <lorenzo_ferron@hotmail.it>
2023-08-01 15:18:39 +0200

    Improve TODO issues

--- src/anfora/agent/index.ts
+++ src/anfora/agent/index.ts
@@ -5,5 +5,8 @@ hookContact();
 hookKeychain();
 // TODO: SSL unpinning
 // TODO: DB password sniffing
-// TODO: JB bypass
+// TODO: JB bypass.
+//  It is not possible to do it using Frida because too many hooks are required.
+//  The best solution is to use a tweak but ALL tweaks changes the behaviour of the app.
+//  Indeed, some functions/syscalls SOMETIMES (not ALWAYS) could be required a patch.
 // TODO: Frida bypass
